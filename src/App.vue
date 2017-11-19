a26559cec13efdb909c84adbc555d6dc4b38a89d
Yaro Schiffelers <yaroschiffelers@gmail.com>
2017-11-19 21:40:16 +0100

    removed some lines

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <headernavys></headernavys>
     <router-view></router-view>
-    <!-- <cta1></cta1> -->
     <footerys></footerys>
   </div>
 </template>
@@ -10,14 +9,12 @@
 <script>
 import headernavys from './components/Headernavys'
 import footerys from './components/Footerys'
-// import cta1 from './design-block-components/cta/Cta1'
 
 export default {
   name: 'app',
   components: {
     'Headernavys': headernavys,
     'Footerys': footerys,
-    // 'Cta1': cta1,
   }
 }
 </script>
